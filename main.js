ce0331ed1cf8080ca677805ac6e288a362ccb777
Cuong Ly <cuonglyy@gmail.com>
2022-08-01 17:11:09 -0700

    added book creation, need to finish adding book to library

--- main.js
+++ main.js
@@ -13,29 +13,29 @@ class Library {
       {
         'title': 'One Piece',
         'author': 'Eiichiro Oda',
-        'pages': '4932943'
+        'pages': '1337'
       },
       {
         'title': 'Gintama',
         'author': 'Hideaki Sorachi',
-        'pages': '23532'
+        'pages': '420'
       }
     ];
   }
 
   //Methods
   bookExists(newBook) {
-    return this.library.some( book => book.title === newBook.title);
+    return this.books.some( book => book.title === newBook.title);
   }
 
   addBook(newBook) {
     if( !this.bookExists(newBook) ) {
-      this.library.push(newBook);
+      this.books.push(newBook);
     }
   }
 
   removeBook(title) {
-    return this.library.filter( book => book.title !== title);
+    return this.books.filter( book => book.title !== title);
   }
 }
 
@@ -43,15 +43,47 @@ const library = new Library();
 
 
 const addBookBtn = document.querySelector("#addBookBtn");
+const bookForm = document.querySelector('.book-form-modal');
 const addBookModal = document.querySelector('.add-book-modal');
 const bookGrid = document.querySelector('#bookGrid');
 const overlay = document.querySelector('.overlay');
+const closeFormBtn = document.querySelector('.form-exit-btn');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const bookReadStatus = document.querySelector('input[name=book_readStatus]:checked');
 
 
-let openAddBookModal = (e) => {
-  console.log(e);
+const openAddBookModal = () => {
+  bookForm.reset();
   addBookModal.classList.add('active');
   overlay.classList.add('active');
  }
 
+const closeAddBookModal = () => {
+  addBookModal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+const createNewBook = () => {
+  const title = bookTitle.textContent;
+  const author = bookAuthor.textContent;
+  const pages = bookPages.textContent;
+  const status = bookReadStatus.value;
+
+  return new Book(title, author, pages, status);
+}
+
+const addBookToLibrary = () => {
+  const newBook = createNewBook();
+
+  if (library.books.bookExists(newBook)) {
+    return 'error, book already exists';
+  }
+
+  
+}
+
+overlay.addEventListener('click', closeAddBookModal);
+closeFormBtn.addEventListener('click', closeAddBookModal);
 addBookBtn.addEventListener('click', openAddBookModal);
